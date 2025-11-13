e3d893468e13f8f2ad136b4a9109b3d452861468
Claudia Cortes <claudiacortes@MacBook-Pro-de-Claudia.local>
2025-11-13 08:30:22 -0600

    Clase 13 noviembre: metoso que retornan y parametros

--- Semana 5/clase_12112025/src/clase_12112025/Clase_12112025.java
+++ Semana 5/clase_12112025/src/clase_12112025/Clase_12112025.java
@@ -13,14 +13,44 @@ import java.util.Scanner;
 public class Clase_12112025 {
     
     public static void main(String[] args) {
+        int continuar=0;
+         Scanner entrada = new Scanner(System.in);
+        while (continuar!=1) {
+             
+              System.out.println("Ingrese su opcion");
+              int op = entrada.nextInt();
+              if(op==1){
+                  EjercicioToLowerCase();
+              }else if(op==2){
+                  Suma();
+              }else if(op==3){
+                  Restar(20,40);
+              }
+        }
         /*
         Metodos: Un fragmento de codigo reutilizable que se puede llamar en cualquier parte del codigo. 
         */
         
-        Scanner entrada = new Scanner(System.in);
+      
         // Utilizar un metodo/ invocar/ llamar 
         // NombreMetodo();
         EjercicioToLowerCase();
+        System.out.println("");
+        int valorFinal = Suma();
+  
+        
+        
+        int numero1 = entrada.nextInt();
+        int numero2= entrada.nextInt();
+        int resultadoResta = Restar(numero1,numero2);
+        resultadoResta=Restar(1,2);
+        resultadoResta=Restar(numero1,120);
+        resultadoResta=Restar(numero2,130);
+        resultadoResta=Restar(873,numero1);
+        resultadoResta=Restar(873,numero2);
+        System.out.println("El resultado es: "+ resultadoResta);
+        
+     
       
     }// fin del main 
     
@@ -33,7 +63,27 @@ public class Clase_12112025 {
     void: no tenemos ningun resultado 
     3) dentro de las llaves del metodo, se programa con normalidad
     4) El metodo, puede estar definidio peor eso no significa que se va a ejecutar, para ello tenemos que llamarlo en el main.
+    5) Podemos crear metodos que nos den un resultado
+        -> return valorRegresar
+        -> al hacer return, el tipo de retorno del metodo debe coincidir con el tipo de dato que estamos usando en el return 
+    6) Parametros y argumentos: 
+        -> parametros: son los valores que se definen como necesarios para que el metodo trabaje
+        -> argumentos: son los valores que se le pasan al metodo en su llamado
     */
+    public static int Restar(int numero1, int numero2){
+        int resultado = numero1-numero2;
+        return resultado;
+    }
+    
+    public static int Suma(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero");
+        int numero1 = entrada.nextInt();
+        System.out.println("Ingrese el segundo numero");
+        int numero2 = entrada.nextInt();
+        int resultado = numero1+numero2;
+        return resultado;
+    }
     
     public static void EjercicioToLowerCase(){
      Scanner entrada = new Scanner(System.in);
