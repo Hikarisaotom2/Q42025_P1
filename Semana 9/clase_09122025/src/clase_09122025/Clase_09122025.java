dab9b65fabfac6ccc58d0223c7aec3dc65b80a2e
Claudia Cortes <claudiacortes@MacBook-Pro-de-Claudia.local>
2025-12-10 08:34:38 -0600

    Clase 10 diciembre: recursion segunda parte

--- Semana 9/clase_09122025/src/clase_09122025/Clase_09122025.java
+++ Semana 9/clase_09122025/src/clase_09122025/Clase_09122025.java
@@ -13,13 +13,31 @@ public class Clase_09122025 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-     /*
-     1 metodo
-        caso base: me ayuda a salir de la recursion 
-        caso recursivo : me ayuda a generar el ciclo 
-     */
+    public static void main(String[] args) {     
+     int resp = factorialRecursivo(4);
+        System.out.println(resp);
     }
+    
+                /*
+    Reglas para prueba de escritorio
+        1) crece de abajo hacia arriba 
+        2)SIEMPRE que llamamemos a un metodos,
+        vamos a ingresar algo a la pila 
+        3) la computadora solamente se concentra 
+        en una cosa a la vez (lo que este en el tope de la pila)
+        4)cuando realicemos un retorno exitoso,
+        vamos a sacar el elemento de la pila
+        */
+    
+    public static int factorialRecursivo(int numero){
+        if(numero==1){/*Caso base*/
+            return 1;
+        }else{ /*Caso recursivo*/
+            // x! = x* (x-1)!
+         return numero*factorialRecursivo(numero-1);  
+        }// fin del if
+    }// fin del metodo
+    
     
     public static int factorialConFor(int numero){
         //3*2*1
@@ -33,13 +51,6 @@ public class Clase_09122025 {
         }*/
         return factorial;
     }
-    
-    public static int factorialRecursivo(int numero){
-        if(numero==1){/*Caso base*/
-            return 1;
-        }else{ /*Caso recursivo*/
-         return numero*factorialRecursivo(numero-1);  
-        }// fin del if 
-    }// fin del metodo 
+ 
     
 }
